58b00ff24ae4420031937b9fbaaba7cc0e4d7d2b
Rohit Singh <rs21Rohit@gmail.com>
2025-01-04 21:26:10 +0530

    .

--- ques07.java
+++ ques07.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class ques07 {
 	public static void main(String args[]){
 		Scanner in = new Scanner(System.in);
+		System.out.println("Enter the number");
 		int a = in.nextInt();
 		if((a%10==0) && (a>99) && (a<1000)){
 			System.out.println("Entered number is 3 digit number and it a multiple of 10 ");
