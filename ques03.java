0ccf4864b7ddc920bd68d0f310769cb6173ec2a4
Rohit Singh <rs21Rohit@gmail.com>
2025-01-04 21:00:22 +0530

    Input Statement

--- ques03.java
+++ ques03.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class ques03 {
 	public static void main(String []args){
 		Scanner in = new Scanner(System.in);
+		System.out.println("Enter the Number");
 		int a = in.nextInt();
 		if(a%3==0 && a%5==0){
 			System.out.println("Yes it is a multiple of both 3 and 5");
