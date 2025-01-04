e7174e1beeeeb1afe131dc1ba08703cc17dba420
Rohit Singh <rs21Rohit@gmail.com>
2025-01-04 21:54:11 +0530

    ques12

---
+++ ques12.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class ques12 {
+public static void main(String[] args) {
+	Scanner in = new Scanner(System.in);
+	System.out.println("Enter the Character");
+	int a = in.nextInt();
+	char b = (char) a;
+	System.out.println(b);
+	
+
+}	
+}
+
+
