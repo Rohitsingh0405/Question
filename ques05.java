5184ee05fb621cb44b151287597225490b33ad17
Rohit Singh <rs21Rohit@gmail.com>
2025-01-04 21:13:31 +0530

    enter the number

--- ques05.java
+++ ques05.java
@@ -3,11 +3,12 @@ import java.util.Scanner;
 public class ques05 {
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
+		System.out.println("Enter the number to check");
 		int a = in.nextInt();
-		int b = a%100;
 		if(a%100==a){
 			System.out.println("The entered number is 2 digit number");
 		}
-		System.out.println(b);
+		
+	
 	}
 }
