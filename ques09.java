4a9307558ebc041f3c3e73075b8409c2436c994b
Rohit Singh <rs21Rohit@gmail.com>
2025-01-04 21:37:51 +0530

    .

--- ques09.java
+++ ques09.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class ques09 {
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
+		System.out.println("Enter the number");
 		int a = in.nextInt();
 		if(a%10==0){
 			System.out.println("The entered number ends with 0 ");
