b622c14939f730ad73d0877261b949164720a159
Rohit Singh <rs21Rohit@gmail.com>
2025-01-04 21:19:47 +0530

    .

--- ques06.java
+++ ques06.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class ques06 {
 	public static void main(String args[]){
 		Scanner in = new Scanner(System.in);
+		System.out.println("Enter the number");
 		int a = in.nextInt();
 		if(a>99 && a<1000)	{
 			System.out.println("The Entered number is a three digit number");
