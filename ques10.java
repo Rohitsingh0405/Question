8b9b3881728067bdeab7394dd9309b44bef4128a
Rohit Singh <rs21Rohit@gmail.com>
2025-01-04 21:49:13 +0530

    .

--- ques10.java
+++ ques10.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class ques10 {
 public static void main(String[] args) {
 	Scanner in = new Scanner(System.in);
+	System.out.println("Enter the Character");
 	char a = in.next().charAt(0);
 	int b = a;
 	if(b>=65 && b<=90){
