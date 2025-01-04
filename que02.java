2dcc76cbbcef0dcd725295733077b58814314902
Rohit Singh <rs21Rohit@gmail.com>
2025-01-04 20:57:40 +0530

    .

--- que02.java
+++ que02.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class que02{
 	public static void main(String []args){
 		Scanner in = new Scanner(System.in);
-		System.out.println("Enter the number");
+		System.out.println("Enter the number to know the multiple of 10 ");
 		int a = in.nextInt();
 		if(a%10==0){
 			System.out.println("Yes it is a multiple of 10 ");
