1ec6266d20b18aba7fa66a9af8bdee45dbe08119
Rohit Singh <rs21Rohit@gmail.com>
2025-01-04 21:18:38 +0530

    QUes06

---
+++ ques06.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class ques06 {
+	public static void main(String args[]){
+		Scanner in = new Scanner(System.in);
+		int a = in.nextInt();
+		if(a>99 && a<1000)	{
+			System.out.println("The Entered number is a three digit number");
+		}
+	}
+}
