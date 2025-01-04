2599e27b16e5ea8f0f917541563b9d582d39c39f
Rohit Singh <rs21Rohit@gmail.com>
2025-01-04 23:08:20 +0530

    ques14

--- ques14.java
+++ ques14.java
@@ -3,6 +3,7 @@ public class ques14 {
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
 		int a = in.nextInt();
+		System.out.println("Enter the character");
 		if(a>=48 && a<58){
 			System.out.println("YES");
 		}
