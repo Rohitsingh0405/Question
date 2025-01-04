f9921dea9af081456098e6e012d815e160e50c26
Rohit Singh <rs21Rohit@gmail.com>
2025-01-04 21:47:50 +0530

    ques10

---
+++ ques10.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class ques10 {
+public static void main(String[] args) {
+	Scanner in = new Scanner(System.in);
+	char a = in.next().charAt(0);
+	int b = a;
+	if(b>=65 && b<=90){
+		System.out.println("It is Upper case Ascii");
+	}
+	else{
+		
+	}
+	
+
+}	
+}
