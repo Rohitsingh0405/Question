e29994a93e596f47f0d56f78fd7272483bf97084
Rohit Singh <rs21Rohit@gmail.com>
2025-01-04 22:56:51 +0530

    ques14

---
+++ ques14.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class ques14 {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int a = in.nextInt();
+		if(a>=48 && a<58){
+			System.out.println("YES");
+		}
+		else{
+			System.out.println("No");
+		}
+	}
+}
