09cf5b0bd2e2fbd97b2d20819b0533657b15faec
Rohit Singh <rs21Rohit@gmail.com>
2025-01-04 21:32:15 +0530

    .

--- ques08.java
+++ ques08.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class ques08 {
 	public static void main(String []args){
 		Scanner in = new Scanner(System.in);
+		System.out.println("Enter the number");
 		int a = in.nextInt();
 		if((a>99) && (a<1000) && (a%2==0) && (a%5==0) && (a%10==0)){
 			System.out.println("Yes");
