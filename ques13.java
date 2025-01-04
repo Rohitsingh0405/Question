1a1d41fbadd265dacf435acc7baae25269fccda8
Rohit Singh <rs21Rohit@gmail.com>
2025-01-04 22:57:18 +0530

    ques13

---
+++ ques13.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class ques13 {
+public static void main(String[] args) {
+	Scanner in = new Scanner(System.in);
+	System.out.println("Enter the Character");
+	char a = in.next().charAt(0);
+	int b = a;
+	System.out.println(b);	
+
+}	
+}
+
+
+
